4d29af716d05292567797479100c6c1946a85da5
rosypandey <119398874+rosypandey@users.noreply.github.com>
2023-05-08 15:35:18 +0545

    Adds block comments

--- myscript.js
+++ myscript.js
@@ -5,19 +5,3 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
-
-  
- 
-
-
-// //function onclick input radio type
-
-// const starsvalue = document.querySelectorAll('.star');
-// let ratingValue;
-// starsvalue.forEach(star => {
-//   star.addEventListener('click', () => {
-//     ratingValue = star.value;
-//     console.log(`Selected rating: ${ratingValue}`);
-//   });
-// });
-
--- testiflow.php
+++ testiflow.php
@@ -1,17 +1,23 @@
 <?php
 /*
  * Plugin Name:      TestiFlow
- * Plugin URI:        https://example.com/plugins/the-basics/
+ * Version:          1.0.0
+ * Plugin URI:       https://example.com/plugins/the-basics/
  * Description:      Try it today and experience the difference!
- * Author:            Rosy Pandey
-  * License:           GPL v2 or later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://example.com/mytestimonials/
+ * Author:           Rosy Pandey
+ * License:          GPL v2 or later
+ * License URI:      https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:       https://example.com/mytestimonials/
+ * TextDomain:       testiflow
  */
 
-
-
-function plg_testimonials_post_type() {
+/**
+ * Registers `TestiFLow` Post Types.
+ * 
+ * @since 1.0.0
+ * @return void
+ */
+function testiflow_post_type() {
     $labels = array(
         'name' => 'TestiFlow',
         'singular_name' => 'TestiFlow',
@@ -32,11 +38,14 @@ function plg_testimonials_post_type() {
      );
     register_post_type('testiflow', $args);
 }
-add_action('init', 'plg_testimonials_post_type');
+add_action('init', 'testiflow_post_type');
 
-//custom taxonomy
+/**
+ * Registers `categorie` taxonomy.
+ * @return void
+ */
 
-function plg_add_new_taxonomy(){
+function testiflow_add_new_taxonomy(){
     $labels = array(
         'name' => 'Testimonial Categories',
         'search_items'      => 'Search Categories' ,
@@ -59,67 +68,78 @@ function plg_add_new_taxonomy(){
     );
     register_taxonomy( 'rp-categorie', ['testiflow'], $args );
 }
-add_action( 'init', 'plg_add_new_taxonomy' );
+add_action( 'init', 'testiflow_add_new_taxonomy' );
 
-//metabox for custom post type
+/**
+ * Registers `PERSONAL INFO` metabox.
+ * @return void
+ */
 
-function plg_add_custom_meta_box() {
+function testiflow_add_custom_meta_box() {
     add_meta_box(
         'contact_meta_box_id',
         'PERSONAL INFO',
-        'plg_contact_meta_box_callback_func',
+        'testiflow_personal_info_metabox_callback',
         'testiflow','normal','default',
-       
     );
 }
-add_action('add_meta_boxes', 'plg_add_custom_meta_box');
+add_action('add_meta_boxes', 'testiflow_add_custom_meta_box');
 
-//callback function of metabox 'PERSONAL INFO'
-function  plg_contact_meta_box_callback_func($post){
+/**
+ * Callback for Personal Info Metabox.
+ * @param WP_Post $post Post Object.
+ */
+function testiflow_personal_info_metabox_callback( $post ) {
  
-   $value1= get_post_meta( $post->ID,"_name", true );
-   $value2= get_post_meta( $post->ID ,"_email", true );
-   $value3= get_post_meta( $post->ID ,"_address", true );
-
+    $value1= get_post_meta( $post->ID,"_name", true );
+    $value2= get_post_meta( $post->ID ,"_email", true );
+    $value3= get_post_meta( $post->ID ,"_address", true );
     ?>
     <form method="POST" name="formdata">
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name" placeholder="enter name" value="<?php echo esc_attr( $value1) ?>">
-        
+        <label for="name"><?php _e( 'Name:', 'testiflow' ); ?></label>
+        <input type="text" id="name" name="name" placeholder="enter name" value="<?php echo esc_attr( $value1) ?>">
 
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" placeholder="enter email" 
-    size="40" value="<?php echo esc_attr( $value2) ?>"/>
+        <label for="email"><?php _e( 'Email:', 'testiflow' ); ?></label>
+        <input type="email" id="email" name="email" placeholder="enter email" 
+        size="40" value="<?php echo esc_attr( $value2) ?>"/>
 
-    <label for="address">Address:</label>
-    <input type="text" id="address" name="address" placeholder="enter address"
-     size="40"  value="<?php echo esc_attr( $value3) ?>" />
+        <label for="address"><?php _e( 'Address:', 'testiflow' ); ?></label>
+        <input type="text" id="address" name="address" placeholder="enter address"
+        size="40"  value="<?php echo esc_attr( $value3) ?>" />
+        <?php wp_nonce_field( 'metabox_form', 'metabox_nonce' ); ?>
     </form>
   <?php
 }
 
-//to save the metadata
-function plg_save_metadata( $post_id ) {
+/**
+ * Save the metadata.
+*/
+
+function testiflow_save_metadata( $post_id ) {
+    if( ! wp_verify_nonce( $_POST['metabox_nonce'],'metabox_form' ) ) {
+        return;
+    };
+
     if ( isset( $_POST['name'] ) ) {
-        update_post_meta( $post_id, '_name', sanitize_text_field( $_POST['name'] ) );
+        update_post_meta( $post_id, '_name', sanitize_text_field( wp_unslash( $_POST['name'] ) ) );
     }
     if ( isset( $_POST['email'] ) ) {
-        update_post_meta( $post_id, '_email', sanitize_text_field( $_POST['email'] ) );
+        update_post_meta( $post_id, '_email', sanitize_text_field( wp_unslash( $_POST['email'] ) ) );
     }
     if ( isset( $_POST['address'] ) ) {
-        update_post_meta( $post_id, '_address', sanitize_text_field( $_POST['address'] ) );
+        update_post_meta( $post_id, '_address', sanitize_text_field( wp_unslash( $_POST['address'] ) ) );
     }
 
 }
-add_action( 'save_post', 'plg_save_metadata' );
+add_action( 'save_post', 'testiflow_save_metadata' );
 
+/**
+ * Display the data of metabox field.
+*/
 
-//shortcode to display the form data
-
-
-function plg_custom_post_type_shortcode( $atts ) {
+function testiflow_custom_post_type_shortcode( $atts ) {
     $args =  array(
-        'post_type' => 'testiflow',
+    'post_type' => 'testiflow',
        'post_per_page'=>10,
        'publish_status'=>'Published');
    
@@ -149,36 +169,35 @@ function plg_custom_post_type_shortcode( $atts ) {
                 </tr>
                 <?php
                   }
+                wp_reset_postdata();
               }
                 ?>
-           
         </table>
     <?php
-
-      
     $output=ob_get_clean();
     return $output;
    
 }
-add_shortcode( 'custom_posts', 'plg_custom_post_type_shortcode' );
+add_shortcode( 'custom_posts', 'testiflow_custom_post_type_shortcode' );
 
+/**
+ * Add custom column to backend list.
+*/
 
-
-
-
-//add custom column to backend list
-function plg_custom_metabox_column($columns){
+function testiflow_custom_metabox_column($columns){
     $columns['name']='name';
     $columns['email']='email';
     $columns['address']='address';
     
     return $columns;
 } 
-add_filter('manage_testiflow_posts_columns','plg_custom_metabox_column');
+add_filter('manage_testiflow_posts_columns','testiflow_custom_metabox_column');
 
-//to display the column field data in custom column
+/**
+ *Display the column field data in custom column.
+*/
 
-function plg_display_custom_metabox_value($column,$post_id){
+function testiflow_display_custom_metabox_value($column,$post_id){
     if ($column === 'name') {
              $value = get_post_meta($post_id, '_name', true);
               echo $value;
@@ -192,43 +211,49 @@ function plg_display_custom_metabox_value($column,$post_id){
           }
 
 }
-add_action('manage_testiflow_posts_custom_column', 'plg_display_custom_metabox_value',10,2);
+add_action('manage_testiflow_posts_custom_column', 'testiflow_display_custom_metabox_value',10,2);
 
+/**
+ *Display the excerpt column data in backend.
+*/
 
-//add Excerpt column to backend list
-
-function plg_custom_excerpt_column( $columns ) {
+function testiflow_custom_excerpt_column( $columns ) {
     $columns['excerpt'] ='Excerpt';
     return $columns;
 }
-add_filter( 'manage_testiflow_posts_columns', 'plg_custom_excerpt_column' );
+add_filter( 'manage_testiflow_posts_columns', 'testiflow_custom_excerpt_column' );
 
-//to display the excerpt column data in backend
 
-function plg_custom_excerpt_column_content( $column_name, $post_id ) {
+
+
+function testiflow_custom_excerpt_column_content( $column_name, $post_id ) {
 
     if ( $column_name == 'excerpt' ) {
         echo get_the_excerpt($post_id);
     }
 }
-add_action( 'manage_testiflow_posts_custom_column', 'plg_custom_excerpt_column_content',10,2);
+add_action( 'manage_testiflow_posts_custom_column', 'testiflow_custom_excerpt_column_content',10,2);
 
 
-//to enqueue the css file
-function plg_my_enqueue_styles() {
+
+/**
+ * Enqueue the css file.
+*/
+
+function testiflow_my_enqueue_styles() {
     wp_enqueue_style( 'my-style', plugins_url( '/css/mystyle.css', __FILE__ ) );
 }
-add_action( 'wp_enqueue_scripts', 'plg_my_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'testiflow_my_enqueue_styles' );
 
+/**
+ * Displays all posts in frontend based on categorie.
+*/
 
-//shortcode to display the all the custom post in frontend
-function plg_cat_listing_func($atts)
+function testiflow_cat_listing_func($atts)
 {$atts = shortcode_atts(
     array(
         'category_name' => '',
-        
     ), $atts, 'lists' );
-
 
     $args = array(
         'post_type' => 'testiflow',
@@ -250,8 +275,6 @@ function plg_cat_listing_func($atts)
                 <div class="row">
     <?php
     if($query->have_posts()):
-
-
         while($query->have_posts()) :
             $query->the_post() ;
            
@@ -279,43 +302,47 @@ function plg_cat_listing_func($atts)
 
 
 }
-add_shortcode('plg_lists','plg_cat_listing_func');
+add_shortcode('testiflow_lists','testiflow_cat_listing_func');
 
+/**
+ * Add a custom column to the Testimonials list table for the feature ima.
+*/
 
-
-
-
-// Add a custom column to the Testimonials list table for the feature image
-add_filter( 'manage_testiflow_posts_columns', 'plg_add_testimonial_image_column' );
-function plg_add_testimonial_image_column( $columns ) {
+add_filter( 'manage_testiflow_posts_columns', 'testiflow_add_testimonial_image_column' );
+function testiflow_add_testimonial_image_column( $columns ) {
     $columns['testimonial_image'] = 'Image';
     return $columns;
 }
 
-// Display the feature image in the custom column on the Testimonials list table
-add_action( 'manage_testiflow_posts_custom_column', 'plg_display_testimonial_image_column', 10, 2 );
-function plg_display_testimonial_image_column( $column, $post_id ) {
+/**
+ * Display the feature image in the custom column on the Testimonials list table.
+*/
+
+add_action( 'manage_testiflow_posts_custom_column', 'testiflow_display_testimonial_image_column', 10, 2 );
+function testiflow_display_testimonial_image_column( $column, $post_id ) {
     if ( 'testimonial_image' === $column ) {
         $thumbnail = get_the_post_thumbnail( $post_id, array( 60, 60 ) );
         echo $thumbnail;
     }
 }
 
-// to enqueue  cdn links of bootstrap
-function plg_mytestimonial_stylesheet()
+/**
+ * Enqueue  cdn links of bootstrap.
+*/
+
+function testiflow_mytestimonial_stylesheet()
 {
     wp_enqueue_style( 'myCSS', plugins_url( 'mystyle.css', __FILE__ ) );
     wp_enqueue_style( 'myplugin-style', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' );
     wp_enqueue_script( 'myplugin-script', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js', null, null, true );
 }
-add_action('wp_enqueue_scripts', 'plg_mytestimonial_stylesheet');
+add_action('wp_enqueue_scripts', 'testiflow_mytestimonial_stylesheet');
 
+/**
+ * function to create the setting submenu.
+*/
 
-
-//creating sub menu called setting
-
-
-function plg_setting_menu_page() //menu page which have settings options for this plugin
+function testiflow_setting_menu_page() //menu page which have settings options for this plugin
 {
     add_submenu_page(
         'edit.php?post_type=testiflow', // the slug of your custom post type
@@ -323,13 +350,17 @@ function plg_setting_menu_page() //menu page which have settings options for thi
         'settings', // the label of the menu item
         'manage_options', // the required user capability to view the menu item
         'settings', // the slug of the menu page
-        'plg_setting_callback_func' // the callback function to display the content of the menu page
+        'testiflow_setting_callback_func' // the callback function to display the content of the menu page
     );
     
 }
-add_action( 'admin_menu', 'plg_setting_menu_page' );
+add_action( 'admin_menu', 'testiflow_setting_menu_page' );
 
-function plg_setting_callback_func(){
+/**
+ * Callback function of setting sub menu.
+*/
+
+function testiflow_setting_callback_func(){
   
    ?>
    <form method='post'>
@@ -337,13 +368,10 @@ function plg_setting_callback_func(){
     <label for="checkbox">Enable user email</label><br>
     <input type="checkbox" id="checkbox2" name="enable_address">
     <label for="checkbox2">Enable user address</label><br>
-   
-
-
     <input type='submit'  id='submit' value='save changes' name='save'/>
 </form>
     <?php
-    //to save datas in database
+    // To save datas in database.
     if( isset($_POST['save']) ) {
         $Postdata=[];
         $Postdata['user_email']=isset($_POST['enable_email']);
@@ -351,31 +379,28 @@ function plg_setting_callback_func(){
         
     }
     
-
 }
 
+/**
+ * link the javacript file and slider cdn link.
+*/
 
-
-
-
-
-
-
-//link the javacript file and slider cdn link
-function plg_testimonial_stylesheet()
+function testiflow_testimonial_stylesheet()
 {
   
-    //swiper slider cdn
+    // Swiper slider cdn.
     wp_enqueue_style( 'swipercss-cdn', 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css' );
     wp_enqueue_script( 'swiperjs_cdn', 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js', null, null, true );
     //custom js
     wp_enqueue_script( 'myscript', plugins_url( 'myscript.js', __FILE__ ), array('jquery'), null, true );
 }
-add_action('wp_enqueue_scripts', 'plg_testimonial_stylesheet');
+add_action('wp_enqueue_scripts', 'testiflow_testimonial_stylesheet');
 
-//shortcode to display the slider
+/**
+ * Shortcode to display the slider.
+*/
 
-function plg_slider_func()
+function testiflow_slider_func()
 {
 
     $args = array(
@@ -394,11 +419,13 @@ function plg_slider_func()
 
 
 }
-add_shortcode('slider_lists','plg_slider_func');
+add_shortcode('slider_lists','testiflow_slider_func');
 
-   
-//to add testimonials through form
-function plg_add_testimonial_form_shortcode() {
+   /**
+    * To add testimonials through form.
+    */
+
+function testiflow_add_testimonial_form_shortcode() {
     ob_start();
     ?>
     <form method="post">
@@ -414,17 +441,17 @@ function plg_add_testimonial_form_shortcode() {
         <label for="testimonial_content">Testimonial:</label><br>
         <textarea name="testimonial_content" id="testimonial_content" rows="5"></textarea><br><br>
         
-            <div class="stars">
-        <input class="star" type="radio" id="star-1" name="rating" value="1"/>
-        <label class="star" for="star-1"></label>
-        <input class="star" type="radio" id="star-2" name="rating" value="2"/>
-        <label class="star" for="star-2"></label>
-        <input class="star" type="radio" id="star-3" name="rating" value="3"/>
-        <label class="star" for="star-3"></label>
-        <input class="star" type="radio" id="star-4" name="rating" value="4"/>
-        <label class="star" for="star-4"></label>
-        <input class="star" type="radio" id="star-5" name="rating" value="5"/>
-        <label class="star" for="star-5"></label>
+        <div class="stars">
+            <input class="star" type="radio" id="star-1" name="rating" value="1"/>
+            <label class="star" for="star-1"></label>
+            <input class="star" type="radio" id="star-2" name="rating" value="2"/>
+            <label class="star" for="star-2"></label>
+            <input class="star" type="radio" id="star-3" name="rating" value="3"/>
+            <label class="star" for="star-3"></label>
+            <input class="star" type="radio" id="star-4" name="rating" value="4"/>
+            <label class="star" for="star-4"></label>
+            <input class="star" type="radio" id="star-5" name="rating" value="5"/>
+            <label class="star" for="star-5"></label>
     </div>
          
              <?php wp_nonce_field( 'testimonial_form', 'mytestimonial_nonce' ); ?>
@@ -436,20 +463,21 @@ function plg_add_testimonial_form_shortcode() {
     $form = ob_get_clean();
     return $form;
 }
-add_shortcode('add_testimonial_shortcode', 'plg_add_testimonial_form_shortcode');
+add_shortcode('add_testimonial_shortcode', 'testiflow_add_testimonial_form_shortcode');
 
+/**
+ *  Handle the form submission
+ * @return void
+ */
 
-
-
-// Handle the form submission
-function plg_add_testimonial_form_submit(){
+function testiflow_add_testimonial_form_submit(){
    
     if (isset($_POST['submit_testimonial'])) {
         if( ! wp_verify_nonce( $_POST['mytestimonial_nonce'],'testimonial_form' ) ) {
             return;
         };
         $testimonial_author = sanitize_text_field($_POST['testimonial_author']);
-        $testimonial_content = sanitize_textarea_field($_POST['testimonial_content']);
+        $testimonial_content = sanitize_textarea_field($_POST['testimonial_content']);  
         $company_name = sanitize_text_field($_POST['company_name']);
         $company_website = sanitize_text_field($_POST['company_website']);
         $rawRating = $_POST['rating'];
@@ -474,19 +502,27 @@ function plg_add_testimonial_form_submit(){
     }
   
 }
-add_action('init','plg_add_testimonial_form_submit'); 
+add_action('init','testiflow_add_testimonial_form_submit'); 
 
-// Add custom columns to testimonial list
-function plg_add_testimonial_columns($columns) {
+/**
+ *  Add custom columns to testimonial list
+ * @return array
+ * @param array $columns Array of column.
+ */
+
+function testiflow_add_testimonial_columns($columns) {
     $columns['company_name'] = 'Company Name';
     $columns['company_website'] = 'Company Website';
     $columns['testimonial_rating'] = 'Rating';
     return $columns;
 }  
-add_filter('manage_testiflow_posts_columns', 'plg_add_testimonial_columns');
+add_filter('manage_testiflow_posts_columns', 'testiflow_add_testimonial_columns');
 
-//  custom columns with meta data
-function plg_add_data_testimonial_columns($column_name, $post_id) {
+/**
+ * Displays the custom column in list of testimonials.
+ */
+
+function testiflow_add_data_testimonial_columns($column_name, $post_id) {
     if ($column_name == 'company_name') {
         $company_name = get_post_meta($post_id, 'company_name', true);
         echo $company_name;
@@ -502,16 +538,16 @@ function plg_add_data_testimonial_columns($column_name, $post_id) {
    
 
 }
-add_action('manage_testiflow_posts_custom_column', 'plg_add_data_testimonial_columns', 10, 2);
+add_action('manage_testiflow_posts_custom_column', 'testiflow_add_data_testimonial_columns', 10, 2);
 
 
+/**
+ * Shortcode to display the testimonials in diffrent view
+ * @return array
+ * @param array The user defined shortcode attributes..
+ */
 
-
-
-
-//shortcode to display the testimonials in list and grid view
-
-function plg_display_func( $atts ) {
+function testiflow_display_func( $atts ) {
     $atts = shortcode_atts( array(
         'view' => 'list', // default view is list
         'per_row' => 1, // default number of items per row is 1
@@ -544,7 +580,7 @@ function plg_display_func( $atts ) {
     $display = ob_get_clean();
     return $display;
 }
-add_shortcode( 'display_lists', 'plg_display_func' );
+add_shortcode( 'display_lists', 'testiflow_display_func' );
 
 
 
